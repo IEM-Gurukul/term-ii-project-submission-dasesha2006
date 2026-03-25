823a0eadd39491c708959605df5712425219ea59
dasesha2006 <dasesha2006@gmail.com>
2026-03-25 17:31:36 +0530

    Fixed line endings and added library logic

---
+++ src/LibraryManagement.java
@@ -0,0 +1,44 @@
+abstract class LibraryItem {
+    protected int id;
+    protected String title;
+
+    public LibraryItem(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    
+    public abstract void display();
+}
+
+
+class Book extends LibraryItem {
+    private String author;
+
+    public Book(int id, String title, String author) {
+        super(id, title);
+        this.author = author;
+    }
+
+ 
+    @Override
+    public void display() {
+        System.out.println("Book ID: " + id + ", Title: " + title + ", Author: " + author);
+    }
+}
+
+class Magazine extends LibraryItem {
+    private int issueNumber;
+
+    public Magazine(int id, String title, int issueNumber) {
+        super(id, title);
+        this.issueNumber = issueNumber;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Magazine ID: " + id + ", Title: " + title + ", Issue: " + issueNumber);
+    }
+}
+
+
