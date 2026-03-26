2e596b5f1b3f9a6e84d492735aeb9605a8cbad21
dasesha2006 <dasesha2006@gmail.com>
2026-03-26 20:52:46 +0530

    Added Person abstract class

--- src/LibraryManagement.java
+++ src/LibraryManagement.java
@@ -1,4 +1,11 @@
-public class LibraryManagement {
-    
+abstract class Person {
+    protected String name;
+    protected int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    abstract void displayDetails();
 }
-g
