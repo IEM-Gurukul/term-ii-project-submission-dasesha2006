1c104c96a53d2e972782b41e5e36ef0d7bd75fbe
dasesha2006 <dasesha2006@gmail.com>
2026-03-27 23:53:41 +0530

    Added book class

--- src/LibraryManagement.java
+++ src/LibraryManagement.java
@@ -13,9 +13,26 @@ class Student extends Person {
     public Student(String name, int id) {
         super(name, id);
     }
-    
+
         @Override
     void displayDetails() {
         System.out.println("Student ID: " + id + ", Name: " + name);
+    }
+}
+class Book {
+    int bookId;
+    String title;
+    String author;
+    boolean isIssued;
+
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+
+    void displayBook() {
+        System.out.println(bookId + " | " + title + " | " + author + " | Issued: " + isIssued);
     }
 }
