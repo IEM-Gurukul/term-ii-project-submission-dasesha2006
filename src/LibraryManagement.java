2df47e40a64b9bd6e339d510e23adb373ea5d475
dasesha2006 <dasesha2006@gmail.com>
2026-03-29 02:35:29 +0530

    Added library class,add book,delete book,search book,issue book,return book, show book

--- src/LibraryManagement.java
+++ src/LibraryManagement.java
@@ -36,3 +36,76 @@ class Book {
         System.out.println(bookId + " | " + title + " | " + author + " | Issued: " + isIssued);
     }
 }
+
+class Library {
+    ArrayList<Book> books = new ArrayList<>();
+    ArrayList<Student> students = new ArrayList<>();
+
+
+    void addBook(Book b) {
+        books.add(b);
+        System.out.println("Book added successfully!");
+    }
+
+    
+    void deleteBook(int id) {
+        books.removeIf(b -> b.bookId == id);
+        System.out.println("Book deleted!");
+    }
+
+    
+    void searchBook(String keyword) {
+        for (Book b : books) {
+            if (b.title.equalsIgnoreCase(keyword) || b.author.equalsIgnoreCase(keyword)) {
+                b.displayBook();
+            }
+        }
+    }
+
+    
+    void issueBook(int bookId) {
+        try {
+            for (Book b : books) {
+                if (b.bookId == bookId) {
+                    if (!b.isIssued) {
+                        b.isIssued = true;
+                        System.out.println("Book issued successfully!");
+                        return;
+                    } else {
+                        throw new Exception("Book already issued!");
+                    }
+                }
+            }
+            throw new Exception("Book not found!");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    
+    void returnBook(int bookId) {
+        try {
+            for (Book b : books) {
+                if (b.bookId == bookId) {
+                    if (b.isIssued) {
+                        b.isIssued = false;
+                        System.out.println("Book returned successfully!");
+                        return;
+                    } else {
+                        throw new Exception("Book was not issued!");
+                    }
+                }
+            }
+            throw new Exception("Book not found!");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    
+    void showBooks() {
+        for (Book b : books) {
+            b.displayBook();
+        }
+    }
+}
